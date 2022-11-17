21e9671b88429c8364e90e7c69d10ae7e48b5358
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-11-17 16:09:31 +0300

    fixed redis counter connect, added docker-compose

--- src/main/java/com/requestcounter/config/CounterConfig.java
+++ src/main/java/com/requestcounter/config/CounterConfig.java
@@ -1,24 +1,31 @@
 package com.requestcounter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class CounterConfig {
 
+    @Value("${spring.redis.host}")
+    private String hostName;
+
+    @Value("${spring.redis.port}")
+    private int port;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
+    public LettuceConnectionFactory redisConnectionFactory() {
 
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(hostName, port));
+    }
 
     @Bean
     public RedisTemplate<String, Integer> redisTemplate() {
         RedisTemplate<String, Integer> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory());
         return template;
     }
 }
