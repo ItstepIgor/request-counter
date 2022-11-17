6510801bc5754a63c3b87245ecbf0274b1fcd53f
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-11-17 20:30:42 +0300

    fixed warnings

--- src/main/java/com/requestcounter/service/CounterService.java
+++ src/main/java/com/requestcounter/service/CounterService.java
@@ -2,24 +2,31 @@ package com.requestcounter.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.support.atomic.RedisAtomicInteger;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
 public class CounterService {
 
-    private final RedisTemplate redisTemplate;
+    private static final String COUNTER_REDIS = "counter-redis";
+
+    private final RedisTemplate<String, Integer> redisTemplate;
 
     private final AtomicInteger counter = new AtomicInteger();
 
 
     public String getCounterRedis(String name) {
-        RedisAtomicInteger counterRedis =
-                new RedisAtomicInteger("counter-redis", redisTemplate.getConnectionFactory());
+        RedisConnectionFactory connectionFactory =
+                Objects.requireNonNull(redisTemplate.getConnectionFactory());
+
+        RedisAtomicInteger counterRedis = new RedisAtomicInteger(COUNTER_REDIS, connectionFactory);
+
         return "\"" + "Hello, " + name + ", " + counterRedis.incrementAndGet() + "\"";
     }
 
